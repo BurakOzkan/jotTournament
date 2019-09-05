eee947d205f5c8f3b884ac96fb8bd98336291239
burak <burakozkan.ceng@gmail.com>
2019-09-05 14:24:38 +0300

    sagas :: Debug

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,7 +1,6 @@
 import { all } from 'redux-saga/effects';
 
 import fetchLastSubmission from './fetchLastSubmission.js';
-import createForm from './createForm.js';
 import fetchAllForms from './fetchAllForms';
 import fetchTournamentTeams from './fetchTournamentTeams';
 
@@ -9,7 +8,6 @@ import fetchTournamentTeams from './fetchTournamentTeams';
 export default function* rootSaga() {
     yield all([
         fetchLastSubmission(),
-        createForm(),
         fetchAllForms(),
         fetchTournamentTeams()
     ]);
